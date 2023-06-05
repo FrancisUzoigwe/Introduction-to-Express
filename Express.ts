56eef7be84fd0d698a560187ae4fa7d9b760c4bc
Francis Uzoigwe <kossyuzoigwe@gmail.com>
2023-06-05 13:31:25 +0100

    Updates

--- Express.ts
+++ Express.ts
@@ -47,14 +47,71 @@
 // })
 
 
+// import express, { Application } from "express";
+
+// const port: number = 2000;
+
+// const app: Application = express();
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Server is now running" + port)
+// })
+
+
+
+// import express, { Application } from "express";
+
+// const port: number = 1000;
+// const app:  Application = express();
+
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Server is now running at CodeLab by SkillHub")
+// })
+
+
+
+// import express, { Application } from "express";
+
+// const port: number = 2030;
+// const app: Application = express();
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Francis started a sever")
+// })
+
+
+
+
+
+// import express, { Application } from "express";
+
+// const port: number = 3001;
+// const app: Application = express();
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Hello World")
+// })
+
+
+
+
 import express, { Application } from "express";
 
-const port: number = 2000;
-
+const port: number = 4000;
 const app: Application = express();
+
 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log("Server is now running" + port)
+    console.log("A server was started by a Full-Stack Engineer")
 })
