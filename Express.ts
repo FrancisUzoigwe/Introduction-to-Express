1c70b992f95ce660db00947531e25279043cc1e8
Francis Uzoigwe <kossyuzoigwe@gmail.com>
2023-06-05 13:02:28 +0100

    updating Express

--- Express.ts
+++ Express.ts
@@ -1,10 +1,60 @@
+// import express, { Application } from "express";
+
+// const port: number = 2002;
+// const app: Application = express();
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//   console.log("listening on port " + port);
+// });
+
+// import express, { Application } from "express";
+
+// const port: number = 2002;
+// const app: Application = express();
+
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Server is now live..")
+// })
+
+
+// import express, { Application } from "express";
+
+// const port: number = 2020;
+// const app: Application = express();
+
+// app.use(express.json());
+// app.listen(port, () => {
+//     console.log("Server is now listening to " + port)
+// })
+
+
+// import express, { Application } from "express";
+
+
+// const port: number = 3000;
+
+// const app: Application =express();
+
+// app.use(express.json());
+
+// app.listen(port, () => {
+//     console.log("Server is now Active on " + port)
+// })
+
+
 import express, { Application } from "express";
 
-const port: number = 2002;
+const port: number = 2000;
+
 const app: Application = express();
 
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log("listening on port " + port);
-});
+    console.log("Server is now running" + port)
+})
